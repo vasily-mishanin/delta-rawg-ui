d7b415d786c747c487bca9fdd22d8e10fdb03420
vasily <vasilymishanin@gmail.com>
2023-11-25 18:41:51 +0600

    init: add styled components configuration

---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+import { Wrapper, Title } from '@/components/examples';
+function Greeting() {
+  return (
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
+}
+export default Greeting;
